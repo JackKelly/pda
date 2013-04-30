642c4982fffb6fd835c4ab0cd4b20ed97f82fa6a
Jack Kelly <jack-list@xlk.org.uk>
2013-04-30 16:50:26 +0100

    Added default constructor. Added feature to on_duration_per_day where user can set an acceptable_dropout_rate

--- pda/channel.py
+++ pda/channel.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 
+SECS_PER_DAY = 60*60*24 # seconds per day
+
 class Channel(object):
     """A single channel of data.
     
@@ -11,42 +13,64 @@ class Channel(object):
         max_sample_period (np.timedelta64, s): The maximum time allowed
             between samples. If data is missing for longer than
             max_sample_period then the appliance is assumed to be off.
+        sample_period (np.timedelta64, s)
     """
     
-    def __init__(self, filename, timezone='Europe/London',
+    DEFAULT_TIMEZONE = 'Europe/London'
+    
+    def __init__(self, filename=None, timezone=DEFAULT_TIMEZONE,
+                 sample_period=None, # seconds
                  max_sample_period=20 # seconds
                  ):
+        self.max_sample_period = np.timedelta64(max_sample_period, 's')
+        if filename is not None:
+            self.load(filename, timezone)
+            if sample_period is None:
+                self.sample_period = (np.diff(self.series.index.values).min()
+                                      .astype('timedelta64[s]'))
+            else:
+                self.sample_period = np.timedelta64(sample_period, 's') 
         
+    def load(self, filename, timezone=DEFAULT_TIMEZONE):
         date_parser = lambda x: dt.datetime.fromtimestamp(x)
         df = pd.read_csv(filename, sep=' ', parse_dates=True,
                          index_col=0, names=['timestamp', 'power'],
                          dtype={'timestamp':np.float64, 'power':np.float64},
                          date_parser=date_parser)
         self.series = df.icol(0).tz_localize(timezone)
-        self.max_sample_period = np.timedelta64(max_sample_period, 's')
 
-    def on_duration_per_day(self, pwr_threshold=5):
+    def on_duration_per_day(self, pwr_threshold=5, 
+                            acceptable_dropout_rate=0.2):
         """
         Args:
             pwr_threshold (float or int): Optional. Threshold which defines the
-                distinction between "on" and "off".  Watts.    
+                distinction between "on" and "off".  Watts.
+            acceptable_dropout_rate (float). Must be >= 0 and <= 1.  Remove any
+                row which has a greater sampling dropout rate.
             
         Returns:
             pd.DataFrame.  One row per day.  Columns:
                 on_duration (np.timedelta64[ns])
                 sample_size (np.int64)
         """
+        
+        assert(0 <= acceptable_dropout_rate <= 1)
 
         # Construct the index for the output.  Each item is a Datetime
         # at midnight.  Get rid of the first & last items so rng represents
         # just the full-days for which we have date.
         rng = pd.date_range(self.series.index[0], self.series.index[-1],
-                            freq='D', normalize=True)[1:-1]
+                            freq='D', normalize=True)
         on_durations = pd.Series(   index=rng, dtype=np.timedelta64)
         sample_sizes = pd.Series(0, index=rng, dtype=np.int64)
 
+        min_samples_per_day = ((SECS_PER_DAY / self.sample_period.astype(np.int64)) * 
+                               (1-acceptable_dropout_rate))
+
         for day in rng:
             data_for_day = self.series[day.strftime('%Y-%m-%d')]
+            if data_for_day.size < min_samples_per_day:
+                continue
             i_above_threshold = np.where(data_for_day[:-1] >= pwr_threshold)[0]
             timedeltas = (data_for_day.index[i_above_threshold+1].values -
                           data_for_day.index[i_above_threshold].values)
@@ -54,5 +78,7 @@ class Channel(object):
             on_durations[day] = timedeltas.sum()
             sample_sizes[day] = data_for_day.size
 
-        return pd.DataFrame({'on_duration':on_durations,
-                             'sample_size':sample_sizes})
+        df = pd.DataFrame({'on_duration':on_durations,
+                           'sample_size':sample_sizes})
+
+        return df.dropna()
