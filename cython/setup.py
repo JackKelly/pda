f40739cfc03086e6f12324d326f530fa0a22cc0a
Jack Kelly <jack-list@xlk.org.uk>
2013-05-07 14:27:28 +0100

    tests work pass again

--- cython/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(ext_modules=cythonize(
-                            "load_pwr_data.pyx",
-                            sources=["load_pwr_data_cpp.cpp"],
-                            language="c++",
-     ))
--- pda/tests/test_channel.py
+++ pda/tests/test_channel.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python
 from __future__ import print_function, division
 import unittest, os, inspect
-from pda.channel import Channel
+from pda.channel import Channel, SECS_PER_HOUR
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
@@ -17,18 +18,18 @@ class TestChannel(unittest.TestCase):
     def test_init(self):
         self.assertIsNotNone(self.channel)
          
-#     def test_plot(self):
-#         print(self.channel.series.tail())
-#         print(self.channel.series.index)
-#         self.channel.series.plot()
-#         plt.show()
+    # def test_plot(self):
+    #     print(self.channel.series.tail())
+    #     print(self.channel.series.index)
+    #     self.channel.series.plot()
+    #     plt.show()
 
     def test_on_duration_per_day(self):
         c = Channel()
         idx = pd.date_range('2013-04-01', '2013-04-05', freq='6S')
-        target_on_duration = np.timedelta64(86394, 's')
+        target_on_duration = 86394 / SECS_PER_HOUR
         c.series = pd.Series(100, index=idx)
-        c.sample_period = np.timedelta64(6, 's')
+        c.sample_period = 6
         df = c.on_duration_per_day()
         self.assertEqual(df['on_duration'][0], target_on_duration)
         self.assertEqual(df['sample_size'][0], 86400/6)
@@ -36,5 +37,6 @@ class TestChannel(unittest.TestCase):
         # c.series.plot()
         # plt.show()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     unittest.main()
