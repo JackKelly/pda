86c6ff23baded5c534838f0a27764514f6a35699
Jack Kelly <jack-list@xlk.org.uk>
2013-05-13 17:07:54 +0100

    making a start on loading all channels as a DataFrame

--- scripts/plot_activity.py
+++ scripts/plot_activity.py
@@ -2,55 +2,53 @@
 from __future__ import print_function, division
 import matplotlib.pyplot as plt
 import pda.dataset as ds
-import pda.stats
 import numpy as np
-from scipy.stats import linregress
 from matplotlib.ticker import MultipleLocator
+from pandas.tseries.offsets import DateOffset
 
 THRESHOLD = 0.005 # remove any days will less kwh or hours on per day
 MIN_DAYS_PER_CHAN = 10
-DATA_DIR = '/data/mine/vadeec/jack-merged/'
-# DATA_DIR  = '/data/mine/vadeec/jack/137'
+#DATA_DIR = '/data/mine/vadeec/jack-merged/'
+DATA_DIR  = '/data/mine/vadeec/jack/137'
 
 print("Loading dataset...")
 dataset = ds.load_dataset(DATA_DIR)
 
-days = pd.date_range(dataset[0].series.index[0], dataset[0].series.index[-1],
-                      freq='D', normalize=True)
+# create pd.DataFrame of all channels
+dct = {}
+for channel in dataset:
+    dct[channel.name] = channel.series
+df = pd.DataFrame(dct)
 
+# TODO:
+# put entire dateset into one big dataframe
+# then I can select time ranges once for entire dataframe
 
-print("Plotting...")
+date_index, day_boundaries = dataset[0].days()
+N_DAYS = date_index.size - 1
+N_CHANNELS = len(dataset)
+MINS_PER_DAY = 24 * 60
+WIDTH = MINS_PER_DAY # 1 pixel per minute
+HEIGHT = N_DAYS * N_CHANNELS
+data = np.zeros((HEIGHT, WIDTH), dtype=np.float)
 
-def plot_heatmap(data, ax):
-    im = ax.imshow(data, interpolation='nearest')
-    ax.tick_params(labelsize=8) # , label2On=True)
-
-    # X axis
-    ax.xaxis.set_major_locator(MultipleLocator(1))
-    ax.set_xticklabels(weather.columns, rotation=90)
-    ax.xaxis.set_ticks_position('both')
-
-    # Y axis
-    ax.yaxis.set_major_locator(MultipleLocator(1))
-    ax.set_yticklabels(names)
-    return im
-
-fig = plt.figure(figsize=(10,10))
-
-ax1 = fig.add_subplot(1,2,1)
-ax1.set_title('Appliance duration vs weather')
-im1 = plot_heatmap(r_sq_hours, ax1)
-fig.colorbar(im1)
-
-ax2 = fig.add_subplot(1,2,2)
-ax2.set_title('Appliance energy consumption vs weather')
-im2 = plot_heatmap(r_sq_kwh, ax2)
-fig.colorbar(im2)
-
-plt.show()
-print("Done")
-
-# Trigger emacs to run this script using the "compile" command
-# ;;; Local Variables: ***
-# ;;; compile-command: "python correlate_weather_dataset.py" ***
-# ;;; end: ***
+import ipdb; ipdb.set_trace()
+for chan_i in range(N_CHANNELS): # "chan_i" means "channel index"
+    print("Loading", dataset[chan_i].name)
+    date_index, day_boundaries = dataset[chan_i].days()
+    for day_i in range(N_DAYS):
+        day_start_index, day_end_index = day_boundaries[day_i]
+        if day_start_index is None:
+            continue
+        data_for_day = dataset[chan_i].series[day_start_index:day_end_index]
+        day = date_index[day_i]
+        for minute_i in range(MINS_PER_DAY):
+            t = day + DateOffset(minutes=minute_i)
+            data_for_minute = dataset[chan_i].series[t.strftime('%Y-%m-%d %H:%M')]
+            if any(data_for_minute > dataset[chan_i].on_power_threshold):
+                y = HEIGHT - chan_i - (day_i * N_CHANNELS) - 1
+                try:
+                    data[y][minute_i] = 1
+                except:
+                    print(minute_i, y)
+                    raise
