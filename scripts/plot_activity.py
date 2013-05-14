c7dd4d6f2a3882724c20216964bc021f2ae49e31
Jack Kelly <jack-list@xlk.org.uk>
2013-05-14 22:48:17 +0100

    small changes

--- scripts/plot_activity.py
+++ scripts/plot_activity.py
@@ -26,31 +26,38 @@ df = pd.DataFrame(dct)
 # TODO:
 # then I can select time ranges once for entire dataframe
 
-period_range, period_boundaries = indicies_of_periods(df.index, 'D')
-N_DAYS = period_range.size - 1
-N_CHANNELS = len(dataset)
+day_range, day_boundaries = indicies_of_periods(df.index, 'D')
+N_DAYS = day_range.size - 1
+N_CHANNELS = df.columns.size
 MINS_PER_DAY = 24 * 60
 WIDTH = MINS_PER_DAY # 1 pixel per minute
 HEIGHT = N_DAYS * N_CHANNELS
-data = np.zeros((HEIGHT, WIDTH), dtype=np.float)
+bitmap = np.zeros((HEIGHT, WIDTH), dtype=np.float)
 
-import ipdb; ipdb.set_trace()
-for chan_i in range(N_CHANNELS): # "chan_i" means "channel index"
-    print("Loading", dataset[chan_i].name)
-#    period_range, period_boundaries = dataset[chan_i].days()
+for day in day_range:
+    try:
+        start_index, end_index = day_boundaries[day]
+    except KeyError:
+        continue
+    data_for_day = df[start_index:end_index]
+    import ipdb; ipdb.set_trace()
+
+
+for chan_name, series in df.iteritems():
+    print("Loading", chan_name)
     for day_i in range(N_DAYS):
-        day_start_index, day_end_index = period_boundaries[day_i]
+        day_start_index, day_end_index = day_boundaries[day_i]
         if day_start_index is None:
             continue
         data_for_day = dataset[chan_i].series[day_start_index:day_end_index]
-        day = period_range[day_i]
+        day = day_range[day_i]
         for minute_i in range(MINS_PER_DAY):
             t = day + DateOffset(minutes=minute_i)
             data_for_minute = dataset[chan_i].series[t.strftime('%Y-%m-%d %H:%M')]
             if any(data_for_minute > dataset[chan_i].on_power_threshold):
                 y = HEIGHT - chan_i - (day_i * N_CHANNELS) - 1
                 try:
-                    data[y][minute_i] = 1
+                    bitmap[y][minute_i] = 1
                 except:
                     print(minute_i, y)
                     raise
