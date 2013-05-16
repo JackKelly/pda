d8dc880f5f06620d4027cfbe0199316137dd4397
Jack Kelly <jack-list@xlk.org.uk>
2013-05-16 12:18:55 +0100

    comments cleanup

--- pda/channel.py
+++ pda/channel.py
@@ -21,7 +21,9 @@ ACCEPTABLE_DROPOUT_RATE_IF_NOT_UNPLUGGED = 0.2
 # data_dir contains a "custom_on_power_thresholds.dat" file
 DEFAULT_ON_POWER_THRESHOLD = 3
 
+# Seconds for each period length.
 SECS_PER_FREQ = {'T':60, 'H': 3600, 'D': 86400, 'M': 2678400, 'A': 31536000}
+
 
 def load_labels(data_dir):
     """Loads data from labels.dat file.
@@ -61,11 +63,85 @@ def load_sometimes_unplugged(data_dir):
 def get_sample_period(series):
     """Find the sample period by finding the stats.mode of the 
     forward difference.  Only use the first 100 samples (for speed).
-    Returns an int
+    Returns period in seconds (float).
     """
     fwd_diff = np.diff(series.index.values[:100]).astype(np.float)
     mode_fwd_diff = stats.mode(fwd_diff)[0][0]
     return mode_fwd_diff / 1E9
+
+
+def indicies_of_periods(datetime_index, freq):
+    """
+    Args:
+        datetime_index (pd.tseries.index.DatetimeIndex)
+
+        freq (str): one of the following:
+            'A' for yearly
+            'M' for monthly
+            'D' for daily
+            'H' for hourly
+            'T' for minutely
+
+    Returns: period_range, period_boundaries:
+
+        period_range (pd.tseries.index.PeriodIndex): in the 
+            local time of datetime_index.
+
+        period_boundaries (dict):
+            keys = pd.Period (in the local time of datetime_index)
+            values = 2-tuples of ints: (start index, end index for period)
+    """
+    try:
+        TZ = datetime_index[0].tz.zone
+    except AttributeError:
+        TZ = None
+
+    # Generate a PeriodIndex object
+    period_range = pd.period_range(datetime_index[0], datetime_index[-1], 
+                                   freq=freq)
+
+    # Declare and initialise some constants and variables used
+    # during the loop...
+
+    # Find the minimum sample period.
+    # For the sake of speed, only use the first 100 samples.
+    FWD_DIFF = np.diff(datetime_index.values[:100]).astype(np.float)
+    MIN_SAMPLE_PERIOD = FWD_DIFF.min() / 1E9
+    MAX_SAMPLES_PER_PERIOD = SECS_PER_FREQ[freq] / MIN_SAMPLE_PERIOD
+    MAX_SAMPLES_PER_2_PERIODS = MAX_SAMPLES_PER_PERIOD * 2
+    n_rows_processed = 0
+    period_boundaries = {}
+    for period in period_range:
+        # The simplest way to get data for just a single period is to use
+        # data_for_day = datetime_index[period.strftime('%Y-%m-%d')]
+        # but this takes about 300ms per call on my machine.
+        # So we take advantage of several features of the data to achieve
+        # a 300x speedup:
+        # 1. We use the fact that the data is sorted in order, hence 
+        #    we can chomp through it in order.
+        # 2. MAX_SAMPLES_PER_PERIOD sets an upper bound on the number of
+        #    datapoints per period.  The code is conservative and uses 
+        #    MAX_SAMPLES_PER_2_PERIODS. We only search through a small subset
+        #    of the available data.
+        end_index = n_rows_processed+MAX_SAMPLES_PER_2_PERIODS
+        rows_to_process = datetime_index[n_rows_processed:end_index]
+
+        end_time = period.end_time
+        # Convert to correct timezone.  Can't use end_time.tz_convert(TZ)
+        # because this chucks out loads of warnings:
+        # "Warning: discarding nonzero nanoseconds".  
+        # To silence these warnings we re-create the
+        # tz_localize function (see pandas/tslib.pyx) to allow us to pass
+        # warn=False to to_pydatetime().
+        end_time = pd.Timestamp(end_time.to_pydatetime(warn=False), tz=TZ)
+        indicies_for_period = np.where(rows_to_process < end_time)[0]
+        if indicies_for_period.size > 0:
+            first_i_for_period = indicies_for_period[0] + n_rows_processed
+            last_i_for_period = indicies_for_period[-1] + n_rows_processed + 1
+            period_boundaries[period] = (first_i_for_period, last_i_for_period)
+            n_rows_processed += last_i_for_period - first_i_for_period
+
+    return period_range, period_boundaries
 
 
 class Channel(object):
@@ -77,8 +153,8 @@ class Channel(object):
         max_sample_period (float): The maximum time allowed
             between samples. If data is missing for longer than
             max_sample_period then the appliance is assumed to be off.
-        sample_period (float)
-        acceptable_dropout_period (float): [0, 1]
+        sample_period (float): seconds
+        acceptable_dropout_rate (float): [0, 1] (0 means no dropouts allowed)
         on_power_threshold (float): watts
         name (str)
         data_dir (str)
@@ -293,76 +369,3 @@ class Channel(object):
         watt_seconds = (td_limited * series.values[:-1]).sum()
         return watt_seconds / 3600000
 
-
-def indicies_of_periods(datetime_index, freq):
-    """
-    Args:
-        datetime_index (pd.tseries.index.DatetimeIndex)
-
-        freq (str): one of the following:
-            'A' for yearly
-            'M' for monthly
-            'D' for daily
-            'H' for hourly
-            'T' for minutely
-
-    Returns: period_range, period_boundaries:
-
-        period_range (pd.tseries.index.PeriodIndex): in the 
-            local time of datetime_index.
-
-        period_boundaries (dict):
-            keys = pd.Period (in the local time of datetime_index)
-            values = 2-tuples of ints: (start index, end index for period)
-    """
-    try:
-        TZ = datetime_index[0].tz.zone
-    except AttributeError:
-        TZ = None
-
-    # Generate a PeriodIndex object
-    period_range = pd.period_range(datetime_index[0], datetime_index[-1], 
-                                   freq=freq)
-
-    # Declare and initialise some constants and variables used
-    # during the loop...
-
-    # Find the minimum sample period.
-    # For the sake of speed, only use the first 100 samples.
-    FWD_DIFF = np.diff(datetime_index.values[:100]).astype(np.float)
-    MIN_SAMPLE_PERIOD = FWD_DIFF.min() / 1E9
-    MAX_SAMPLES_PER_PERIOD = SECS_PER_FREQ[freq] / MIN_SAMPLE_PERIOD
-    MAX_SAMPLES_PER_2_PERIODS = MAX_SAMPLES_PER_PERIOD * 2
-    n_rows_processed = 0
-    period_boundaries = {}
-    for period in period_range:
-        # The simplest way to get data for just a single period is to use
-        # data_for_day = datetime_index[period.strftime('%Y-%m-%d')]
-        # but this takes about 300ms per call on my machine.
-        # So we take advantage of several features of the data to achieve
-        # a 300x speedup:
-        # 1. We use the fact that the data is sorted in order, hence 
-        #    we can chomp through it in order.
-        # 2. MAX_SAMPLES_PER_PERIOD sets an upper bound on the number of
-        #    datapoints per period.  The code is conservative and uses 
-        #    MAX_SAMPLES_PER_2_PERIODS. We only search through a small subset
-        #    of the available data.
-        end_index = n_rows_processed+MAX_SAMPLES_PER_2_PERIODS
-        rows_to_process = datetime_index[n_rows_processed:end_index]
-
-        end_time = period.end_time
-        # Convert to correct timezone.  Can't use end_time.tz_convert(TZ)
-        # because this chucks out loads of warnings:
-        # "Warning: discarding nonzero nanoseconds".  
-        # To silence these warnings we re-create the
-        # tz_localize function (see pandas/tslib.pyx) to allow us to pass
-        # warn=False to to_pydatetime().
-        end_time = pd.Timestamp(end_time.to_pydatetime(warn=False), tz=TZ)
-        indicies_for_period = np.where(rows_to_process < end_time)[0]
-        if indicies_for_period.size > 0:
-            first_i_for_period = indicies_for_period[0] + n_rows_processed
-            last_i_for_period = indicies_for_period[-1] + n_rows_processed + 1
-            period_boundaries[period] = (first_i_for_period, last_i_for_period)
-            n_rows_processed += last_i_for_period - first_i_for_period
-
-    return period_range, period_boundaries
