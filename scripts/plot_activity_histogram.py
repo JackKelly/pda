da1fea3a96fdd471982aa6a340dfa926f8137767
Jack Kelly <jack-list@xlk.org.uk>
2013-05-17 15:25:32 +0100

    format histograms correctly no matter what this time span and bin size

--- scripts/plot_activity_histogram.py
+++ scripts/plot_activity_histogram.py
@@ -9,7 +9,8 @@ import datetime
 
 START_DATE = None # datetime.datetime(year=2013, month=3, day=1)
 END_DATE = None # datetime.datetime(year=2013, month=3, day=1)
-BIN_SIZE = 'T'  # H (hourly) or T (minutely)
+BIN_SIZE = 'T' # D (daily) or H (hourly) or T (minutely)
+TIMESPAN = 'D' # D (daily) or W (weekly)
 c = Channel('/data/mine/vadeec/jack-merged', 3)
 
 fig = plt.figure()
@@ -26,23 +27,34 @@ if START_DATE:
     c.series = c.series[c.series.index >= START_DATE]
 if END_DATE:
     c.series = c.series[c.series.index <= END_DATE]
-distribution = c.activity_distribution(bin_size=BIN_SIZE)
+
+distribution = c.activity_distribution(bin_size=BIN_SIZE, timespan=TIMESPAN)
 
 x = np.arange(distribution.size)
 
 COLOR = 'b'
 ax.bar(x, distribution.values, facecolor=COLOR, edgecolor=COLOR)
-ax.set_xlim([0, distribution.size])
-ax.xaxis.set_major_locator(MultipleLocator(distribution.size / 12))
 
-ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_time))
+if TIMESPAN == 'D':
+    BINS_PER_SUBTIMESPAN = 0 if BIN_SIZE=='T' else 1
+    ax.xaxis.set_major_locator(MultipleLocator(distribution.size / 12))
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_time))
+elif TIMESPAN == 'W': # weekly
+    BINS_PER_SUBTIMESPAN = 24 if BIN_SIZE=='H' else 1
+    ax.xaxis.set_major_locator(MultipleLocator(BINS_PER_SUBTIMESPAN))
+    ax.set_xticklabels(['M', 'T', 'W', 'T', 'F', 'S', 'S'])
+
+ax.set_xlim([0, distribution.size])
+ax.set_xticks([tick + (0.4*BINS_PER_SUBTIMESPAN) for tick in ax.get_xticks()])
+ax.set_xlim([0, distribution.size])
 
 date_format = '%d-%m-%Y'
-timespan = ' from ' + c.series.index[0].strftime(date_format)
-timespan += ' to ' + c.series.index[-1].strftime(date_format)
-ax.set_title('Daily activity histogram for ' + c.name.replace('_', ' ') + timespan)
-
-ax.set_xlabel('hour')
+title = "Daily" if TIMESPAN=='D' else "Weekly"
+title += ' activity histogram for ' + c.name.replace('_', ' ')
+title += ' from ' + c.series.index[0].strftime(date_format)
+title += ' to ' + c.series.index[-1].strftime(date_format)
+ax.set_title(title)
+ax.set_xlabel('hour' if TIMESPAN == 'D' else 'day')
 ax.set_ylabel('count')
 
 plt.show()
