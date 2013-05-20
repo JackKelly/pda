5c6465595c046f4aa13b3e9bb4c5d67c8c7432ae
Jack Kelly <jack-list@xlk.org.uk>
2013-05-20 09:53:25 +0100

    just making some notes

--- pda/dataset.py
+++ pda/dataset.py
@@ -27,3 +27,25 @@ def load_dataset(data_dir):
 
     return channels
 
+
+def cluster_appliances(df):
+    """Args:
+       df (pd.DataFrame)
+
+Notes:
+This is ML so need to make sure we're not overfitting.  Split dataset.
+
+Using K-Means: Unfortunately there is no general theoretical
+solution to find the optimal number of clusters for any given data
+set. A simple approach is to compare the results of multiple runs with
+different k classes and choose the best one according to a given
+criterion (for instance the Schwarz Criterion - see Moore's slides[1]),
+but we need to be careful because increasing k results in smaller
+error function values by definition, but also an increasing risk of
+overfitting.)
+
+
+[1]: http://home.deib.polimi.it/matteucc/Clustering/tutorial_html/kmeans.html#moore
+    """
+
+    
