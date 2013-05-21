7578e651df508b8413995de54dedecbbf9e16858
Jack Kelly <jack-list@xlk.org.uk>
2013-05-21 12:13:49 +0100

    clustering for a single day seems to work well.  Must now decode sets of appliances in each cluster.

--- pda/dataset.py
+++ pda/dataset.py
@@ -2,10 +2,12 @@ from __future__ import print_function, division
 from pda.channel import Channel, load_labels
 import sys
 import numpy as np
+import pandas as pd
 from scipy.spatial import distance
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
 import matplotlib.pyplot as plt
+from itertools import cycle
 
 def load_dataset(data_dir):
     """Loads an entire dataset directory.
@@ -36,23 +38,9 @@ def load_dataset(data_dir):
 def cluster_appliances(dataset):
     """Args:
            dataset (list of pda.channel.Channels)
-
-Notes:
-This is ML so need to make sure we're not overfitting.  Split dataset.
-
-Using K-Means: Unfortunately there is no general theoretical
-solution to find the optimal number of clusters for any given data
-set. A simple approach is to compare the results of multiple runs with
-different k classes and choose the best one according to a given
-criterion (for instance the Schwarz Criterion - see Moore's slides[1]),
-but we need to be careful because increasing k results in smaller
-error function values by definition, but also an increasing risk of
-overfitting.)
-
-
-[1]: http://home.deib.polimi.it/matteucc/Clustering/tutorial_html/kmeans.html#moore
-
-http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#example-cluster-plot-dbscan-py
+    
+    Relevant docs:
+    http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html
     """
 
     merged_events = pd.Series()
@@ -63,34 +51,52 @@ http://scikit-learn.org/stable/auto_examples/cluster/plot_dbscan.html#example-cl
         merged_events = merged_events.append(events)
 
     merged_events.sort_index()
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(merged_events.index, merged_events.values, 'o')
-    plt.show()
 
-    x = merged_events.index.astype(int)
+    # distance.pdist() requires a 2D array so convert
+    # datetimeIndex to a 2D array
+    x = merged_events.index.astype(int) / 1E9
     x2d = np.zeros((len(x), 2))
     x2d[:,0] = x
 
+    # Calculate square distance vector
     D = distance.squareform(distance.pdist(x2d))
-    S = 1 - (D / np.max(D)) # similarities
 
-    db = DBSCAN(eps=0.95, min_samples=1).fit(S)
+    # Run cluster algorithm
+    # eps is the maximum distance between samples.  In our case,
+    # it is in units of seconds.
+    db = DBSCAN(eps=60*60, min_samples=2, metric="precomputed").fit(D)
     core_samples = db.core_sample_indices_
     labels = db.labels_
 
     # Number of clusters in labels, ignoring noise if present.
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
-#    import ipdb; ipdb.set_trace()
-
     print('Estimated number of clusters: {:d}'.format(n_clusters_))
-    # print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels)
-    # print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels)
-    # print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels)
-    # print("Adjusted Rand Index: %0.3f" % \
-    #     metrics.adjusted_rand_score(labels_true, labels)
-    # print("Adjusted Mutual Information: %0.3f" % \
-    #     metrics.adjusted_mutual_info_score(labels_true, labels)
-    # print("Silhouette Coefficient: %0.3f" %
-    #        metrics.silhouette_score(D, labels, metric='precomputed'))
+    print("Silhouette Coefficient: {:0.3f}".format(
+           metrics.silhouette_score(D, labels, metric='precomputed')))
+
+    # PLOT
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    colors = cycle('bgrcmybgrcmybgrcmybgrcmy')
+    for k, col in zip(set(labels), colors):
+        if k == -1:
+            # Black used for noise.
+            col = 'k'
+            markersize = 6
+        class_members = [index[0] for index in np.argwhere(labels == k)]
+        cluster_core_samples = [index for index in core_samples
+                                if labels[index] == k]
+        for index in class_members:
+            plot_x = merged_events.index[index]
+            if index in core_samples and k != -1:
+                markersize = 14
+            else:
+                markersize = 6
+            ax.plot(plot_x, 1, 'o', markerfacecolor=col,
+                    markeredgecolor='k', markersize=markersize)
+
+    plt.show()
+
+#    import ipdb; ipdb.set_trace()
